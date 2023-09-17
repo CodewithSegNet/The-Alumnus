798217ad330f49484b1571a1a08c3b69a4dc12a5
segunUb18 <eeomo@yahoo.com>
2023-09-17 05:53:58 +0100

    :construction: Create the user Model

---
+++ app/Models/user_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Import Dependencies
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Initialize Sqlalchemy
+db = SQLAlchemy()
+
+
+# Define the Model
+class UserProfile(db.Model):
+    ''' A class that defines the userProfile
+    '''
+    alumni_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    middle_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    grad_year = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
+    confirm_password = db.Column(db.String(15), nullable=False)
